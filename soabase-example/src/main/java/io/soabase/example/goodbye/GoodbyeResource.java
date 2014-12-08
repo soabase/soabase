0cb2db2643ea7db43e6204c840670467b30a282b
randgalt <jordan@jordanzimmerman.com>
2014-12-07 21:22:10 -0500

    wip

--- soabase-example/src/main/java/io/soabase/example/goodbye/GoodbyeResource.java
+++ soabase-example/src/main/java/io/soabase/example/goodbye/GoodbyeResource.java
@@ -1,23 +1,13 @@
 package io.soabase.example.goodbye;
 
-import io.soabase.core.SoaFeatures;
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/goodbye")
 public class GoodbyeResource
 {
-    private final SoaFeatures features;
-
-    @Inject
-    public GoodbyeResource(SoaFeatures features)
-    {
-        this.features = features;
-    }
-
     @GET
-    public String getHello()
+    public String getGoodbye() throws Exception
     {
         return "goodbye";
     }

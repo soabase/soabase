41ae72e2697e6bb2f08ac03af5c507d813aefe35
randgalt <jordan@jordanzimmerman.com>
2015-02-11 10:15:29 -0500

    fixed javadoc errors

--- soabase-guice/src/main/java/io/soabase/guice/JerseyGuiceModule.java
+++ soabase-guice/src/main/java/io/soabase/guice/JerseyGuiceModule.java
@@ -90,12 +90,12 @@ public class JerseyGuiceModule extends AbstractModule
 
     /**
      * <h3>Servlet Mapping EDSL</h3>
-     * <p/>
+     *
      * <p> Part of the EDSL builder language for configuring servlets
      * and filters with guice-servlet. Think of this as an in-code replacement for web.xml.
      * Filters and servlets are configured here using simple java method calls. Here is a typical
      * example of registering a filter when creating your Guice injector:
-     * <p/>
+     *
      * <pre>
      *   Guice.createInjector(..., new ServletModule() {
      *
@@ -105,28 +105,28 @@ public class JerseyGuiceModule extends AbstractModule
      *     }
      *   }
      * </pre>
-     * <p/>
+     *
      * This registers a servlet (subclass of {@code HttpServlet}) called {@code MyServlet} to service
      * any web pages ending in {@code .html}. You can also use a path-style syntax to register
      * servlets:
-     * <p/>
+     *
      * <pre>
      *       <b>serve("/my/*").with(MyServlet.class)</b>
      * </pre>
-     * <p/>
+     *
      * Every servlet (or filter) is required to be a singleton. If you cannot annotate the class
      * directly, you should add a separate {@code bind(..).in(Singleton.class)} rule elsewhere in
      * your module. Mapping a servlet that is bound under any other scope is an error.
-     * <p/>
-     * <p/>
+     *
+     *
      * <h3>Initialization Parameters</h3>
-     * <p/>
+     *
      * Servlets (and filters) allow you to pass in init params
      * using the {@code <init-param>} tag in web.xml. You can similarly pass in parameters to
      * Servlets and filters registered in Guice-servlet using a {@link java.util.Map} of parameter
      * name/value pairs. For example, to initialize {@code MyServlet} with two parameters
      * ({@code name="Dhanji", site="google.com"}) you could write:
-     * <p/>
+     *
      * <pre>
      *  Map&lt;String, String&gt; params = new HashMap&lt;String, String&gt;();
      *  params.put("name", "Dhanji");
@@ -135,27 +135,27 @@ public class JerseyGuiceModule extends AbstractModule
      *  ...
      *      serve("/*").with(MyServlet.class, <b>params</b>)
      * </pre>
-     * <p/>
-     * <p/>
+     *
+     *
      * <h3>Binding Keys</h3>
-     * <p/>
+     *
      * You can also bind keys rather than classes. This lets you hide
      * implementations with package-local visbility and expose them using
      * only a Guice module and an annotation:
-     * <p/>
+     *
      * <pre>
      *  ...
      *      filter("/*").through(<b>Key.get(Filter.class, Fave.class)</b>);
      * </pre>
-     * <p/>
+     *
      * Where {@code Filter.class} refers to the Servlet API interface and {@code Fave.class} is a
      * custom binding annotation. Elsewhere (in one of your own modules) you can bind this
      * filter's implementation:
-     * <p/>
+     *
      * <pre>
      *   bind(Filter.class)<b>.annotatedWith(Fave.class)</b>.to(MyFilterImpl.class);
      * </pre>
-     * <p/>
+     *
      * See {@link com.google.inject.Binder} for more information on binding syntax.
      */
     protected void configureServlets()
